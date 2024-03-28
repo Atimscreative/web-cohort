8bbc54fae9ebfa59bd3494f9f00f84be5b656a74
atimscreative <timilehinsunday144@gmail.com>
2024-03-28 16:13:34 +0100

    error fixing

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST["email"]) && isset(($_POST["password"]) && isset($_POST["full_name"]))) {
+if (isset($_POST["email"]) && isset(($_POST["password"])) && isset($_POST["full_name"])) {
     $full_name = $_POST["full_name"];
     $email = $_POST["email"];
     $password = $_POST["password"];
@@ -7,24 +7,19 @@ if(isset($_POST["email"]) && isset(($_POST["password"]) && isset($_POST["full_na
     $country = $_POST["country"];
     $age = $_POST["age"];
 
-    $response = array("status" => "success","message" => "You have successfully registered")
-    header("HTTP/1.0 201 OK")
-    echo json_encode($response)
-}elseif (empty($_POST["email"]) || empty(($_POST["password"]) || empty($_POST["full_name"])))
-{
-$response = array(
-    "status" => "failed","message" => "All fields are required"
-)
- header("HTTP/1.0 403 Forbidden")
-    echo json_encode($response)
+    $response = array("status" => "success", "message" => "You have successfully registered");
+    header("HTTP/1.0 201 OK");
+    echo json_encode($response);
+} elseif (empty($_POST["email"]) || empty(($_POST["password"])) || empty($_POST["full_name"])) {
+    $response = array(
+        "status" => "failed", "message" => "All fields are required"
+    );
+    header("HTTP/1.0 403 Forbidden");
+    echo json_encode($response);
 } else {
     $response = array(
-    "status" => "success","failed" => "All fields are required"
-)
- header("HTTP/1.0 403 Forbidden")
-    echo json_encode($response)
+        "status" => "success", "failed" => "All fields are required"
+    );
+    header("HTTP/1.0 403 Forbidden");
+    echo json_encode($response);
 }
-
-
-
-?>
